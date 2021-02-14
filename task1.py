8c670bac278a2bf946b3df1ada25988258febfdd
James U <tenseventyseven@gmail.com>
2021-02-14 22:28:39 +1100

    my solutions

--- task1.py
+++ task1.py
@@ -1,12 +1,40 @@
 #!/usr/bin/env python3
 import csv
+import re
 
 fastq_filenames = []
 fastq_total_bytes = 0
 bam_filenames = []
 bam_total_bytes = 0
 
-# TODO
+with open("files.txt") as csvfile:
+    reader = csv.reader(csvfile, delimiter="|")
+
+    # Skip header
+    next(reader)
+
+    for row in reader:
+        # Skip empty rows
+        if len(row) == 0:
+            continue
+
+        # Skip comments
+        if row[0].startswith("#"):
+            continue
+
+        # Parse row
+        filename = row[1]
+        bytes = int(row[2])
+
+        # FastQs
+        if re.match(r"^.*filtered.*fastq.gz$", filename):
+            fastq_filenames.append(filename)
+            fastq_total_bytes += bytes
+
+        # BAMs
+        if filename.endswith(".bam"):
+            bam_filenames.append(filename)
+            bam_total_bytes += bytes
 
 print(f"FastQ files: {fastq_filenames}, total bytes: {fastq_total_bytes}\n")
 print(f"BAM files: {bam_filenames}, total bytes: {bam_total_bytes}\n")
--- task2.py
+++ task2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 import requests
 
-# TODO
+payload = {"tags": "wisdom|happiness", "limit": "5"}
+r = requests.get("https://api.quotable.io/quotes", params=payload)
+results = r.json()["results"]
+
+for i, r in enumerate(results, start=1):
+    print(f"{i}. {r['content']} -- {r['author']}\n")
